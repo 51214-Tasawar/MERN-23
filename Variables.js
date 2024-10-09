78f3b7634378319477b7376726b29559fcc7ef49
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-09 20:08:37 +0500

    Testing Demo

---
+++ Variables.js
@@ -0,0 +1,27 @@
+// Constent Variables 
+const a = 10 ;
+console.log(a)  // a =10
+
+{
+    const a = 20   // cannot  reassign if Type not mention
+    console.log(a)   // a = 20 
+}
+console.log(a) // a = 10 
+   
+{
+    console.log("Testing ")
+}
+// Let Variables 
+{
+    let a = 10
+    console.log(a)   // a =10
+    {
+         a = 20  // can reassign without Type mention 
+         console.log(a)  // a=20
+    }
+    console.log(a)   // a=20 
+}
+
+{
+    // var is globally Scope  
+}
