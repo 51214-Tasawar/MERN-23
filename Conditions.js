2d526a1fe858e2c14fd4a12be8f4d8ea8b636841
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-15 16:21:13 +0500

    Testing

--- Conditions.js
+++ Conditions.js
@@ -1,12 +1,55 @@
- a = 24 ;
-if (typeof a == "number"){
-    if(a>0){
-        console.log("Positive Number")
+console.log("Counting the days of Month")
+a =2023 ;
+b=2
+if(a%4==0){
+    console.log("This Is Year Of Leap")
+    switch(b){
+     case 1 :
+     case 3 :
+     case 5 :
+     case 7 :
+     case 8 :
+     case 10 :
+     case 12 :
+        console.log("This is  month of 31 Day");
+        break ;
+        case 2:
+        console.log("This is month of 29 Days") ;
+        break ;
+        case 1 :
+            case 4 :
+            case 6 :
+            case 9 :
+            case 11 :
+                console.log("This is month of 30 Days") ;
+             break;
+             default :
+             console.log ("Invalid Input");
+             break ;
     }
-    else{
-        console.log("Negative Number")
-    }
-}
-else{
-    console.log("Enter Valid Number")
+} else{
+    switch(b){
+        case 1 :
+        case 3 :
+        case 5 :
+        case 7 :
+        case 8 :
+        case 10 :
+        case 12 :
+           console.log("This is  month of 31 Day");
+           break ;
+           case 2:
+           console.log("This is month of 28 Days") ;
+           break ;
+           case 1 :
+               case 4 :
+               case 6 :
+               case 9 :
+               case 11 :
+                   console.log("This is month of 30 Days") ;
+                break;
+                default :
+                console.log ("Invalid Input");
+                break ;
+       }
 }
