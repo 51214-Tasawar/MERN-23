f6a5fc67f58f19f6d32b9ccc87b7ee470013307c
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-16 10:25:27 +0500

    My Practice

---
+++ Array.js
@@ -0,0 +1,8 @@
+let arr = ["Tasawar" , 123 , 85,7887,"Inshal Khan" ];
+// console.log(arr) 
+let arr1 = arr ;
+arr1[5]="Naqi Khan" ;
+// console.log(arr) 
+console.log(arr.length);
+arr[6]="Testing Demo"
+console.log(arr1)
