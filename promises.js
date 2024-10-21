1c22c5a58f73dca242556023bd09d1644fb6a955
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-21 20:56:05 +0500

    pushing new value

--- promises.js
+++ promises.js
@@ -1,14 +1,22 @@
 // 
 const promises1 = new Promise((res , rej)=>{
     const a = 15 ;
-    if(a%2== 0){
+    if( a%2== 0){
     res("Even")
     }
     rej("odd")
 })
-
 promises1.then((res)=>{
 console.log(res)
 }).catch((rej)=>{
     console.log(rej)
+})
+const stopWatch = new Promise((res)=>{
+setTimeout=()=>{
+new Date ();
+},4000
+})
+
+stopWatch.then(()=>{
+console.log(res)
 })
