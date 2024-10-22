7908c34cf5db41f557b664ad12636b7fef59c3c8
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-22 07:39:02 +0500

    Update

--- promises.js
+++ promises.js
@@ -1,12 +1,34 @@
-const Promise1 = new Promise((res , rej)=>{
-    a = 9 ;
-    if(a%2==0)
-        res("Even Number");
-    rej("The Given Number is odd")
+// const Promise1 = new Promise((res , rej)=>{
+//     a = 9 ;
+//     if(a%2==0)
+//         res("Even Number");
+//     rej("The Given Number is odd")
+// })
+
+// Promise1.then((result)=>{
+// console.log(result)
+// }).catch((error)=>{
+// console.log(error)
+// })
+
+/*    Using async and await for asynchrounous programming
+const Watch = new Promise((res)=>{
+  setTimeout(()=>{
+    res(new Date ())
+  },1000)
+})
+const stopWatch = new Promise((res)=>{
+  setTimeout(()=>{
+    res(new Date ())
+  },6000)
 })
 
-Promise1.then((result)=>{
-console.log(result)
-}).catch((error)=>{
-console.log(error)
-})
+const Main = async()=>{
+    await stopWatch.then((result)=>{
+        console.log(result)
+    })
+    await Watch.then((result)=>{
+        console.log(result)
+    })
+}
+Main() */
