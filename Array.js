38660a28719db07f7063be58688088729feb2885
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-10-24 20:50:25 +0500

    Update Array

--- Array.js
+++ Array.js
@@ -1,32 +1,38 @@
-{
+ {
     //Arrays are itrative
-let arr = [1 ,2 ,3 ,4 ,5]
+// let arr = [1 ,2 ,3 ,4 ,5]
  
-let arr1 = arr ; // Assigning arr to arr1
+// let arr1 = arr ; // Assigning arr to arr1
 
-arr1[5]="Testing Demo" // changes in arr1 also change in arr 
+// arr1[5]="Testing Demo" // changes in arr1 also change in arr 
 // console.log(arr) // Output [ 1, 2, 3, 4, 5, 'Testing Demo' ]
-arr.shift();
+// arr.shift();
 // console.log(arr1)  // changes in arr also change in arr1 output of arr1 [ 2, 3, 4, 5, 'Testing Demo' ]
 /*
      * Removes the first element from an array and returns it.
      * If the array is empty, undefined is returned and the array is not modified.
  */
-arr.unshift(1)
+// arr.unshift(1)
 // console.log(arr1)  // Output [ 1, 2, 3, 4, 5, 'Testing Demo' ]
 //Inserts new elements at the start of an array, and returns the new length of the array.
 // console.log(arr.includes(3));  // Determines whether an array includes a certain element
 // console.log(arr.indexOf(3));  // Returns the index of the first occurrence of a value in an array
 // arr.push("Test") //Appends new elements to the end of an array
-arr.pop() //Removes the last element from an array and returns it
-console.log(arr)
+// arr.pop() //Removes the last element from an array and returns it
+// console.log(arr)
 
-const [a,b,...c]=arr1; //De Structring an-Array and using Rest Operators 
-console.log(a ,b,c) 
-}
-const arr =["Name" , 12 , null , "Undefined" , false ]
-console.log(arr)
+// const [a,b,...c]=arr1; //De Structring an-Array and using Rest Operators 
+// console.log(a ,b,c) 
+// }
+// const arr =["Name" , 12 , null , "Undefined" , false ]
+// console.log(arr)
 
-const [a,b ,c] = arr ;
+// const [a,b ,c] = arr ;
 
-console.log(`1si is ${a} , 2nd is ${b} 3rd is ${c}`);
+// console.log(`1si is ${a} , 2nd is ${b} 3rd is ${c}`);
+ }
+
+const array = [1,2,3,4,5,6,7,8,9]
+console.log(array)
+console.log(array.unshift())
+// console.log(array.unshift)
