cd3ac78c631b2c6a1d1a18eacba13b980766f7bc
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-15 11:56:35 +0500

    Include and Exclude Attibutes

--- ToDoList/models/usermodel.js
+++ ToDoList/models/usermodel.js
@@ -22,7 +22,10 @@ const getAll=async (body)=>{
   try{
   const user = await model.users.findAll(
     {
-    attributes : ["name" , "username" , ], // Show the Mention Attribute
+    // attributes : ["name" , "username" , "email"], // Show the Mention Attribute
+    attributes : {
+      exclude : ["name" , "username" , "email"]
+    },
     paranoid : false
   }
   )
