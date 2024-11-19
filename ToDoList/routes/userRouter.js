8be06fe50bdc8f03f67d5df3fb5941250a274e0b
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-19 21:11:43 +0500

    Correct Code

--- ToDoList/routes/userRouter.js
+++ ToDoList/routes/userRouter.js
@@ -6,6 +6,6 @@ const {Create ,Update ,Delete ,Get} = require("../Validations/uservalidator")
 routes.post("/createUser",Create,createUser);
 routes.get("/getUsers" , Get,getUsers);
 routes.patch("/updateUser" ,Update,updateUser) ;
-routes.delete("/deleteUser" , DeleteNew,deleteUser);
+routes.delete("/deleteUser" , Delete ,deleteUser);
 
 module.exports = routes ;
