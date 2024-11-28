80f4e1f58bb8bcfdf68384fea780bb7049da337c
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-28 11:54:30 +0500

    Get Update

--- ToDoList/models/definations/productverations.js
+++ ToDoList/models/definations/productverations.js
@@ -1,38 +1,38 @@
-// const {DataTypes , Model} = require("sequelize") ;
-// const sequelize = require("../../dbConnection");
-// const {v4 : uuid} = require("uuid")
+const {DataTypes , Model} = require("sequelize") ;
+const sequelize = require("../../dbConnection");
+const {v4 : uuid} = require("uuid")
 
-// class productsverations extends Model { } ;
+class productsverations extends Model { } ;
 
-// users.init ({
-//     productsverationId :{
-//         primaryKey : true ,
-//         type : DataTypes.STRING(100),
-//     },
-//     veration:{
-//         type : DataTypes.STRING(100) ,
-//         allowNull : false
-//     },
-//     description :{
-//         type : DataTypes.STRING(),
-//         allowNull : false
-//     },
-//     quantity :{
-//         type : DataTypes.NUMBER(),
-//         allowNull : false
-//     },
-//     price : {
-//           type : DataTypes.NUMBER(),
-//           allowNull : false
-//     } 
-// },{
-//     name : "productsverations" ,
-//     timestamps : true ,
-//     paranoid : true,
-//     sequelize : sequelize
-// });
-// productsverations.beforeCreate(async(productsveration)=>{
-//     productsveration.productsverationId = uuid()
-// })
+users.init ({
+    productsverationId :{
+        primaryKey : true ,
+        type : DataTypes.STRING(100),
+    },
+    veration:{
+        type : DataTypes.STRING(100) ,
+        allowNull : false
+    },
+    description :{
+        type : DataTypes.STRING(),
+        allowNull : false
+    },
+    quantity :{
+        type : DataTypes.NUMBER(),
+        allowNull : false
+    },
+    price : {
+          type : DataTypes.NUMBER(),
+          allowNull : false
+    } 
+},{
+    name : "productsverations" ,
+    timestamps : true ,
+    paranoid : true,
+    sequelize : sequelize
+});
+productsverations.beforeCreate(async(productsveration)=>{
+    productsveration.productsverationId = uuid()
+})
 
-// module.exports = productsverations; 
+module.exports = productsverations; 
