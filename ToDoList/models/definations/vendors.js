e8064f74530c5f3a51919e99375ba7bbd15de00f
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-28 11:55:05 +0500

    Update Code

--- ToDoList/models/definations/vendors.js
+++ ToDoList/models/definations/vendors.js
@@ -1,24 +1,24 @@
-// const {DataTypes , Model} = require("sequelize") ;
-// const sequelize = require("../../dbConnection");
-// const {v4 : uuid} = require("uuid");
-// const { allow } = require("joi");
+const {DataTypes , Model} = require("sequelize") ;
+const sequelize = require("../../dbConnection");
+const {v4 : uuid} = require("uuid");
+const { allow } = require("joi");
 
-// class vendors extends Model { } ;
+class vendors extends Model { } ;
 
-// users.init ({
-//     vendorId :{
-//         primaryKey : true ,
-//         type : DataTypes.STRING(100),
-//     }
-// },{
-//     name : "vendors" ,
-//     timestamps : true ,
-//     paranoid : true,
-//     sequelize : sequelize
-// });
-// vendors.beforeCreate(async(vendor)=>{
-//     vendor.vendorId = uuid()
-// })
+users.init ({
+    vendorId :{
+        primaryKey : true ,
+        type : DataTypes.STRING(100),
+    }
+},{
+    name : "vendors" ,
+    timestamps : true ,
+    paranoid : true,
+    sequelize : sequelize
+});
+vendors.beforeCreate(async(vendor)=>{
+    vendor.vendorId = uuid()
+})
 
 
-// module.exports = vendors ; 
+module.exports = vendors ; 
