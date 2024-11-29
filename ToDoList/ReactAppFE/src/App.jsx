b933147da4ac724253a3752bb5d17b277b73a65d
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-11-29 21:21:42 +0500

    Update My Code

--- ToDoList/ReactAppFE/src/App.jsx
+++ ToDoList/ReactAppFE/src/App.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App=()=>{
+  const [count , setcount ] = useState(0);
   return(
     <>
-    This Is for Front End Practice
+   <div className="flex justify-center items-center w-screen h-screen">
+   <h3 >Items : {count}</h3>
+   <button  className="bg-green-700 text-white border-4 "
+   onClick={()=>setcount(count+1)}> Click For Addition</button>
+   <button  className="bg-red-600 text-white border-4"
+  onClick={()=>setcount(count - 1)}>Click For Substraction</button>
+   </div>
     </>
   )
 }
