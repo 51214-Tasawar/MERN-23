3a00398bec867a8237143a2080705c46e47c0c89
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-12-02 20:46:23 +0500

    Commit to check error

--- ToDoList/models/definations/attributes.js
+++ ToDoList/models/definations/attributes.js
@@ -1,29 +1,29 @@
-const {DataTypes , Model} = require("sequelize") ;
-const sequelize = require("../../dbConnection");
-const {v4 : uuid} = require("uuid")
+// const {DataTypes , Model} = require("sequelize") ;
+// const sequelize = require("../../dbConnection");
+// const {v4 : uuid} = require("uuid")
 
-class attributes extends Model { } ;
+// class attributes extends Model { } ;
 
-attributes.init ({
-    attributeId :{
-        primaryKey : true ,
-        type : DataTypes.STRING(100),
-    },
-   name:{
-        type : DataTypes.STRING(100)
-    },
-    value :{
-        type : DataTypes.STRING()
-    }
-},{
-    name : "attributes" ,
-    timestamps : true ,
-    paranoid : true,
-    sequelize : sequelize
-});
-attributes.beforeCreate(async(attribute)=>{
-    attribute.attributeId = uuid()
-})
+// attributes.init ({
+//     attributeId :{
+//         primaryKey : true ,
+//         type : DataTypes.STRING(100),
+//     },
+//    name:{
+//         type : DataTypes.STRING(100)
+//     },
+//     value :{
+//         type : DataTypes.STRING()
+//     }
+// },{
+//     name : "attributes" ,
+//     timestamps : true ,
+//     paranoid : true,
+//     sequelize : sequelize
+// });
+// attributes.beforeCreate(async(attribute)=>{
+//     attribute.attributeId = uuid()
+// })
 
 
-module.exports = attributes ; 
+// module.exports = attributes ; 
