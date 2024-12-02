c5d9c98a461c67a0dfad2967fd2e07fada3203e2
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-12-02 20:45:03 +0500

    Tables Updated

--- ToDoList/models/definations/products.js
+++ ToDoList/models/definations/products.js
@@ -1,40 +1,40 @@
-const {DataTypes , Model} = require("sequelize") ;
-const sequelize = require("../../dbConnection");
-const vendors = require("./vendors")
-const {v4 : uuid} = require("uuid")
+// const {DataTypes , Model} = require("sequelize") ;
+// const sequelize = require("../../dbConnection");
+// const vendors = require("./vendors")
+// const {v4 : uuid} = require("uuid")
 
-class products extends Model { } ;
+// class products extends Model { } ;
 
-products.init ({
-    productId :{
-        primaryKey : true ,
-        type : DataTypes.STRING(100),
-    },
-    productName:{
-        type : DataTypes.STRING(100) ,
-        allowNull : false
-    },
-    description :{
-        type : DataTypes.STRING(),
-        allowNull : false
-    } ,
-    vendorId  : {
-        type : DataTypes.STRING(),
-        allowNull : false ,
-        // references :{
-        //     model :vendors ,
-        //     key : "vendorId "
-        // }
-    }
-},{
-    name : "products" ,
-    timestamps : true ,
-    paranoid : true,
-    sequelize : sequelize
-});
-products.beforeCreate(async(product)=>{
-    product.productId = uuid()
-})
+// products.init ({
+//     productId :{
+//         primaryKey : true ,
+//         type : DataTypes.STRING(100),
+//     },
+//     productName:{
+//         type : DataTypes.STRING(100) ,
+//         allowNull : false
+//     },
+//     description :{
+//         type : DataTypes.STRING(),
+//         allowNull : false
+//     } ,
+//     vendorId  : {
+//         type : DataTypes.STRING(),
+//         allowNull : false ,
+//         // references :{
+//         //     model :vendors ,
+//         //     key : "vendorId "
+//         // }
+//     }
+// },{
+//     name : "products" ,
+//     timestamps : true ,
+//     paranoid : true,
+//     sequelize : sequelize
+// });
+// products.beforeCreate(async(product)=>{
+//     product.productId = uuid()
+// })
 
 
-module.exports = products ; 
+// module.exports = products ; 
