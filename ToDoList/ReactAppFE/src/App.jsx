ac8582bb61f2c46bb3c4a2e59ca27568dea2e701
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2024-12-03 09:50:10 +0500

    Directory Created For

--- ToDoList/ReactAppFE/src/App.jsx
+++ ToDoList/ReactAppFE/src/App.jsx
@@ -4,13 +4,7 @@ const App=()=>{
   const [count , setcount ] = useState(0);
   return(
     <>
-   <div className="flex justify-center items-center w-screen h-screen flex-col">
-   <h3 >Items : {count}</h3>
-   <button  className="bg-green-700 text-white border-4 "
-   onClick={()=>setcount(count+1)}> Click For Addition</button>
-   <button  className="bg-red-600 text-white border-4"
-  onClick={()=>setcount(count - 1)}> Click For Substraction</button>
-   </div>
+   
     </>
   )
 }
